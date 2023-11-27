7955ad7c3fa464cdb6c481e4ae1275ecaee347f4
Nikita Masaev <nik.masaev@mail.ru>
2023-11-27 04:05:38 +0300

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ export { Content } from './content';
 export { Container } from './container';
 export { SideBar } from './sideBar';
 export { ChatsBlock } from './chatsBlock';
-export { ProfileLink } from './ProfileLink';
+export { ProfileLink } from './profileLink';
 export { Avatar } from './avatar';
 export { Header } from './header';
 export { ContextMenu } from './contextMenu';
